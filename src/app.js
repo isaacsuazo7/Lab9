3609dcebd20af3521da9591b377ec8ba2771c652
isaacsuazo7@gmail.com <isaacsuazo7>
2019-09-12 23:27:13 -0600

    Version 1.0.0

---
+++ src/app.js
@@ -0,0 +1,30 @@
+//Servidor
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const moongose = require('mongoose');
+
+//Conectar Base de Datos
+moongose.connect('mongodb://localhost/crud-mongo')
+    .then(db => console.log('DB connected'))
+    .catch(err => console.log(err));
+
+//Importar rutas
+const indexRoutes= require('./routes/index');
+//Configuracion
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+//routes
+app.use('/',indexRoutes);
+
+//Iniciar el servidor
+app.listen(app.get('port'), ()=>{
+    console.log(`Server on port ${app.get('port')}`);
+})
